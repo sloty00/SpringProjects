2cb643d49ae5a6fd3dc1b0f10974763b5ca258ad
sloty <sloty@sloty001.debian>
2018-03-25 01:33:42 -0300

    Prueba con git

---
+++ EjemploGIT/src/main/java/com/developers/EjemploGitApplication.java
@@ -0,0 +1,12 @@
+package com.developers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploGitApplication.class, args);
+	}
+}
